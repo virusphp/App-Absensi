bd77bb694509d4652a9bef72240542bf02cbd57b
virusphp <packercyber@gmail.com>
2021-03-17 08:24:17 +0700

    update tanggal lahir dan tempat lahir:

--- app/Repository/Akun/Akun.php
+++ app/Repository/Akun/Akun.php
@@ -84,7 +84,7 @@ class Akun
                 $akun = DB::connection('sqlsrv_sms')
                     ->table('akun as a')
                     ->select('a.kd_pegawai','a.mac_address','device','a.status_update','a.created_at','updated_at','a.api_token',
-                            'p.nama_pegawai','p.gelar_depan','p.gelar_belakang','p.foto','su.kd_sub_unit','su.nama_sub_unit')
+                            'p.nama_pegawai', 'p.tempat_lahir','p.tgl_lahir','p.gelar_depan','p.gelar_belakang','p.foto','su.kd_sub_unit','su.nama_sub_unit')
                     ->join('dbsimrs.dbo.pegawai as p', 'a.kd_pegawai','=', 'p.kd_pegawai')
                     ->join('dbsimrs.dbo.sub_unit as su', 'p.kd_sub_unit','=', 'su.kd_sub_unit')
                     ->where('a.kd_pegawai', $params->kode_pegawai)
