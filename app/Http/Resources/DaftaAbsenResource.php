eccd6664eba60a7eca78618ae51964c3e4ec5504
virusphp <packercyber@gmail.com>
2021-06-03 07:55:55 +0700

    update key jadwal dan absen

--- app/Http/Resources/DaftaAbsenResource.php
+++ app/Http/Resources/DaftaAbsenResource.php
@@ -15,22 +15,22 @@ class DaftaAbsenResource extends JsonResource
     public function toArray($request)
     {
         if ($this->status_absen == 1) {
-            $key = "jam_masuk";
+            $key = "jam_jadwal";
             $value = $this->jam_masuk;
         } else {
-            $key = "jam_keluar";
+            $key = "jam_jadwal";
             $value = $this->jam_keluar;
         } 
         return [
                 'tanggal'       => tanggal($this->tanggal),
                 'jam'           => waktu($this->jam),
                 $key            => waktu($value),
-                'jam_berangkat' => waktu($this->jam),
+                'jam_absen'     => waktu($this->jam),
                 'selisih'       => selisih($value, $this->jam),
                 'keterangan'    => keterangan($value, $this->jam, $this->status_absen),
                 'status_absen'  => absensi($this->status_absen),
                 'nama_unit'     => $this->nama_sub_unit,
-                'generate_key'  => generateKey($this->generate_key),
+                'generate_key'  => generateKey($this->generate_key)
         ];
     }
 }
